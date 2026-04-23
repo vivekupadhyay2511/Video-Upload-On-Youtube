506ee3432154d26464e16c414ec1f35f427bcbc1
Deepak Upadhyay <upadhyayvivek25@gmail.com>
2026-04-23 21:33:06 +0530

    vercel python setup

---
+++ vercel-python-setup.py
@@ -0,0 +1,8 @@
+# This file is here to trick Vercel into including Python 3 in the deployment environment.
+# yt-dlp requires python3 to run.
+
+def main():
+    print("Python environment triggered.")
+
+if __name__ == "__main__":
+    main()
